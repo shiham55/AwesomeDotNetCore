241953013bab914920e84cb0420faedc7c12bfce
Shiham M <shiham.mohamed@sg.ey.com>
2019-09-15 16:39:26 +0800

    Initial commit

---
+++ AwesomeDotNetCore.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AwesomeDotNetCore.Models;
+using AwesomeDotNetCore.Data;
+using AwesomeDotNetCore.Data.Models;
+
+namespace AwesomeDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        AdventureWorks2017Context _context = new AdventureWorks2017Context();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Page()
+        {
+            return View(_context.Store.ToList());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AwesomeDotNetCore.Web/WebAPI/StoresController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AwesomeDotNetCore.Data;
+using AwesomeDotNetCore.Data.Models;
+
+namespace AwesomeDotNetCore.WebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController : ControllerBase
+    {
+        private readonly AdventureWorks2017Context _context;
+
+        public StoresController(AdventureWorks2017Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Store>>> GetStore()
+        {
+            return await _context.Store.ToListAsync();
+        }
+
+        // GET: api/Stores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Store>> GetStore(int id)
+        {
+            var store = await _context.Store.FindAsync(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return store;
+        }
+
+        // PUT: api/Stores/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStore(int id, Store store)
+        {
+            if (id != store.BusinessEntityId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(store).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stores
+        [HttpPost]
+        public async Task<ActionResult<Store>> PostStore(Store store)
+        {
+            _context.Store.Add(store);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (StoreExists(store.BusinessEntityId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetStore", new { id = store.BusinessEntityId }, store);
+        }
+
+        // DELETE: api/Stores/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Store>> DeleteStore(int id)
+        {
+            var store = await _context.Store.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            _context.Store.Remove(store);
+            await _context.SaveChangesAsync();
+
+            return store;
+        }
+
+        private bool StoreExists(int id)
+        {
+            return _context.Store.Any(e => e.BusinessEntityId == id);
+        }
+    }
+}
