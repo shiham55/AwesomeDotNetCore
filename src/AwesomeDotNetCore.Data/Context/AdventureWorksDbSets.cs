07a874f41ea8cb89038204a555afed1352987fc6
Shiham M <shiham.mohamed@sg.ey.com>
2019-12-16 18:11:31 +0800

    code cleanup

---
+++ src/AwesomeDotNetCore.Data/Context/AdventureWorksDbSets.cs
@@ -0,0 +1,89 @@
+﻿using AwesomeDotNetCore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeDotNetCore.Data.Context
+{
+    public partial class AdventureWorks
+    {
+        #region DbSets
+        public virtual DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public virtual DbSet<ApplicationUserRole> ApplicationUserRole { get; set; }
+
+        public virtual DbSet<Address> Address { get; set; }
+        public virtual DbSet<AddressType> AddressType { get; set; }
+        public virtual DbSet<AwbuildVersion> AwbuildVersion { get; set; }
+        public virtual DbSet<BillOfMaterials> BillOfMaterials { get; set; }
+        public virtual DbSet<BusinessEntity> BusinessEntity { get; set; }
+        public virtual DbSet<BusinessEntityAddress> BusinessEntityAddress { get; set; }
+        public virtual DbSet<BusinessEntityContact> BusinessEntityContact { get; set; }
+        public virtual DbSet<ContactType> ContactType { get; set; }
+        public virtual DbSet<CountryRegion> CountryRegion { get; set; }
+        public virtual DbSet<CountryRegionCurrency> CountryRegionCurrency { get; set; }
+        public virtual DbSet<CreditCard> CreditCard { get; set; }
+        public virtual DbSet<Culture> Culture { get; set; }
+        public virtual DbSet<Currency> Currency { get; set; }
+        public virtual DbSet<CurrencyRate> CurrencyRate { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<DatabaseLog> DatabaseLog { get; set; }
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<EmailAddress> EmailAddress { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<EmployeeDepartmentHistory> EmployeeDepartmentHistory { get; set; }
+        public virtual DbSet<EmployeePayHistory> EmployeePayHistory { get; set; }
+        public virtual DbSet<ErrorLog> ErrorLog { get; set; }
+        public virtual DbSet<Illustration> Illustration { get; set; }
+        public virtual DbSet<JobCandidate> JobCandidate { get; set; }
+        public virtual DbSet<Location> Location { get; set; }
+        public virtual DbSet<Password> Password { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<PersonCreditCard> PersonCreditCard { get; set; }
+        public virtual DbSet<PersonPhone> PersonPhone { get; set; }
+        public virtual DbSet<PhoneNumberType> PhoneNumberType { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategory { get; set; }
+        public virtual DbSet<ProductCostHistory> ProductCostHistory { get; set; }
+        public virtual DbSet<ProductDescription> ProductDescription { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventory { get; set; }
+        public virtual DbSet<ProductListPriceHistory> ProductListPriceHistory { get; set; }
+        public virtual DbSet<ProductModel> ProductModel { get; set; }
+        public virtual DbSet<ProductModelIllustration> ProductModelIllustration { get; set; }
+        public virtual DbSet<ProductModelProductDescriptionCulture> ProductModelProductDescriptionCulture { get; set; }
+        public virtual DbSet<ProductPhoto> ProductPhoto { get; set; }
+        public virtual DbSet<ProductProductPhoto> ProductProductPhoto { get; set; }
+        public virtual DbSet<ProductReview> ProductReview { get; set; }
+        public virtual DbSet<ProductSubcategory> ProductSubcategory { get; set; }
+        public virtual DbSet<ProductVendor> ProductVendor { get; set; }
+        public virtual DbSet<PurchaseOrderDetail> PurchaseOrderDetail { get; set; }
+        public virtual DbSet<PurchaseOrderHeader> PurchaseOrderHeader { get; set; }
+        public virtual DbSet<SalesOrderDetail> SalesOrderDetail { get; set; }
+        public virtual DbSet<SalesOrderHeader> SalesOrderHeader { get; set; }
+        public virtual DbSet<SalesOrderHeaderSalesReason> SalesOrderHeaderSalesReason { get; set; }
+        public virtual DbSet<SalesPerson> SalesPerson { get; set; }
+        public virtual DbSet<SalesPersonQuotaHistory> SalesPersonQuotaHistory { get; set; }
+        public virtual DbSet<SalesReason> SalesReason { get; set; }
+        public virtual DbSet<SalesTaxRate> SalesTaxRate { get; set; }
+        public virtual DbSet<SalesTerritory> SalesTerritory { get; set; }
+        public virtual DbSet<SalesTerritoryHistory> SalesTerritoryHistory { get; set; }
+        public virtual DbSet<ScrapReason> ScrapReason { get; set; }
+        public virtual DbSet<Shift> Shift { get; set; }
+        public virtual DbSet<ShipMethod> ShipMethod { get; set; }
+        public virtual DbSet<ShoppingCartItem> ShoppingCartItem { get; set; }
+        public virtual DbSet<SpecialOffer> SpecialOffer { get; set; }
+        public virtual DbSet<SpecialOfferProduct> SpecialOfferProduct { get; set; }
+        public virtual DbSet<StateProvince> StateProvince { get; set; }
+        public virtual DbSet<Store> Store { get; set; }
+        public virtual DbSet<TransactionHistory> TransactionHistory { get; set; }
+        public virtual DbSet<TransactionHistoryArchive> TransactionHistoryArchive { get; set; }
+        public virtual DbSet<UnitMeasure> UnitMeasure { get; set; }
+        public virtual DbSet<Vendor> Vendor { get; set; }
+        public virtual DbSet<WorkOrder> WorkOrder { get; set; }
+        public virtual DbSet<WorkOrderRouting> WorkOrderRouting { get; set; }
+
+        // Unable to generate entity type for table 'Production.ProductDocument'. Please see the warning messages.
+        // Unable to generate entity type for table 'Production.Document'. Please see the warning messages.
+        #endregion
+    }
+}
