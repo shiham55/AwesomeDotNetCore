448b47ecc2deb3e246ece2e81c9d0fd6b57621c2
Shiham M <shiham.mohamed@sg.ey.com>
2019-12-27 18:46:09 +0800

    email service added

---
+++ src/AwesomeDotNetCore.Service/Common/Email/EmailService.cs
@@ -0,0 +1,20 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace AwesomeDotNetCore.Service.Common
+{
+    public class EmailService : IEmailService
+    {
+        public SendGridClient SendGridClient { get; set; }
+
+        public EmailService(string ApiKey)
+        {
+            this.SendGridClient = new SendGridClient(ApiKey);
+        }        
+
+        public async void Send(SendGridMessage message)
+        {           
+            var response = await SendGridClient.SendEmailAsync(message);
+        }
+    }
+}
